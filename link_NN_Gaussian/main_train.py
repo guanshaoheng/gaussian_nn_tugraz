9fadfa0e7c17eaed40530ca4cc9a5d9e56c6e3df
guanshaoheng <191359602@qq.com>
2023-03-07 17:57:51 +0100

    The first commit.

--- link_NN_Gaussian/main_train.py
+++ link_NN_Gaussian/main_train.py
@@ -1,9 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from network import net_basic
-import numpy as np
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from dataset_gen_gaussian import gaussian_data_gen
 
 '''
     Here is a script referred to  
@@ -16,20 +14,21 @@ from dataset_gen_gaussian import gaussian_data_gen
 
 
 def main():
-    x, y = dataset_gen(num_points=10, noise=0.2)
-    loss_list = []
-    mode_list = ['physics_informed', 'vallina',  'physics_constrained']
+    x, y = dataset_gen(num_points=10, noise=0.15)
+    mode_list = ['vallina', 'physics_informed', 'physics_constrained']
+    loss_dic = {}
     for mode in mode_list:
-        print('\n\n' + '='*60 + '\n' + '\tMode: %s' % mode + '\n')
-        loss_list.append(single_train(x=x, y=y, mode=mode))
-    plot_loss(mode_list=mode_list, loss_array=np.array(loss_list))
-    x, y = dataset_gen(num_points=10, noise=0.2)
+        print('\n\n' + '=' * 60 + '\n' + '\tMode: %s' % mode + '\n')
+        loss_dic[mode] = single_train(x=x, y=y, mode=mode, width=10)
+    #
+    plot_loss(mode_list=mode_list, loss_dic=loss_dic)
+    #
     test(x=x, y=y, mode_list=mode_list)
 
 
-def single_train(x, y, num_epoch=20000, mode='physics_constrained'):
+def single_train(x, y, num_epoch=50000, mode='physics_constrained', width=10):
     # x, y = dataset_gen()
-    model = net_basic(mode=mode, width=10)
+    model = net_basic(mode=mode, width=width)
     optim = torch.optim.Adam(model.parameters())
     loss_operator = torch.nn.MSELoss()
     x_tensor, y_tensor = torch.from_numpy(x).float(), torch.from_numpy(y).float()
@@ -41,42 +40,54 @@ def single_train(x, y, num_epoch=20000, mode='physics_constrained'):
             loss = loss_operator(y_tensor, y_pre)
         else:
             y_pre, physics_item = model.forward_ddy(x_tensor)
-            loss = loss_operator(y_pre, y_tensor) + 1.0 * loss_operator(physics_item, torch.zeros_like(physics_item))
+            loss = loss_operator(y_pre, y_tensor) + \
+                   1.0 * loss_operator(physics_item, torch.zeros_like(physics_item))
         loss.backward()
         optim.step()
         if epoch % 100 == 0:
+            loss = loss_operator(y_pre, y_tensor)
             print('Epoch %d The current loss is %.3e %s' % (epoch, loss.item(), mode))
             loss_list.append([epoch, loss.item()])
     torch.save(model, f='%s.pt' % mode)
-    return loss_list
+    return np.array(loss_list)
 
 
-def dataset_gen(num_points=10, noise=0.2):
-    x = np.linspace(0, np.pi*2., num_points)
-    y = np.sin(x + 0.5) + np.random.random(num_points)*noise
+def dataset_gen(num_points=10, noise=0.05):
+    x = np.linspace(0, np.pi * 2., num_points)
+    y = data_function(x) + np.random.normal(size=num_points) * noise
     # x, y_list = gaussian_data_gen(num_samples=1, num_points=num_points)
     # y = y_list[0] + np.random.random(num_points)*noise
     return np.array(x[:, np.newaxis]), np.array(y[:, np.newaxis])
 
 
-def test(x, y, mode_list: list, ):
+def data_function(x):
+    return np.sin(x + 0.5)
+
+
+def test(x, y, mode_list: list, num_points=50):
     # x, y = dataset_gen()
+    xx = np.linspace(0, np.pi * 2., num_points).reshape(-1, 1)
     data_dic = {}
+    data_dic['TruthWithNoise'] = [x, y]
     for mode in mode_list:
         model = torch.load('%s.pt' % mode)
-        x_tensor, y_tensor = torch.from_numpy(x).float(), torch.from_numpy(y).float()
+        x_tensor = torch.from_numpy(xx).float()
         y_pre = model.forward(x_tensor).detach().numpy()
-        data_dic[mode] = [x, y, y_pre]
+        data_dic[mode] = [xx, data_function(xx), y_pre]
+    xx = np.linspace(0, np.pi * 2., 100)
+    plt.plot(xx, np.sin(xx + 0.5), 'k.', label='TruthWithoutNoise')
     plot_comparison(data_dic)
 
 
 def plot_comparison(data_dic):
     keys = list(data_dic.keys())
-    x, y = data_dic[keys[0]][0], data_dic[keys[0]][1]
-    num_point = len(x)
-    index = list(range(0, num_point, 20)) if num_point>20 else list(range(num_point))
-    plt.scatter(x[index].flatten(), y[index].flatten(), color='r', label='GroudTruth')
     for key in keys:
+        if key == 'TruthWithNoise':
+            x, y = data_dic[key][0], data_dic[key][1]
+            num_point = len(x)
+            index = list(range(0, num_point, 20)) if num_point > 20 else list(range(num_point))
+            plt.scatter(x[index].flatten(), y[index].flatten(), color='r', label='TruthWithNoises')
+            continue
         x, y_pre = data_dic[key][0], data_dic[key][2]
         plt.plot(x, y_pre, label=key)
     plt.legend()
@@ -85,10 +96,11 @@ def plot_comparison(data_dic):
     plt.show()
 
 
-def plot_loss(mode_list, loss_array):
-    n = len(mode_list)
-    for i in range(n):
-        plt.plot(loss_array[i][:, 0], loss_array[i][:, 1], label=mode_list[i])
+def plot_loss(mode_list, loss_dic: dict):
+    for mode in mode_list:
+        epoch = loss_dic[mode][:, 0]
+        loss = loss_dic[mode][:, 1]
+        plt.plot(epoch, loss, label=mode)
     plt.yscale('log')
     plt.legend()
     plt.tight_layout()
